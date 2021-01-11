7275840b926496863ab1198726436d75a5a255e2
bepis <luqdek@gmail.com>
2021-01-11 19:44:24 +0000

    Added a check to see if the keys correspond with a letter or not

--- scripts.rb
+++ scripts.rb
@@ -1,5 +1,13 @@
 def caesar_cipher(str,key)
     str = str.bytes #convert the string into an array of the converted ascii keys
+    str.map! do |item|
+        if item>=65 && item<=90
+
+        elsif item>=97 && item<=122
+        
+        end
+        
+    end
 
 end
 
