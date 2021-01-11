0494e0445b73a412ad37d7836e797aa1abcc6794
bepis <luqdek@gmail.com>
2021-01-11 19:52:50 +0000

    Added the same check to ensure wrapping can happen with both lowercase and uppercase

--- scripts.rb
+++ scripts.rb
@@ -1,20 +1,27 @@
 def caesar_cipher(str,key)
     str = str.bytes #convert the string into an array of the converted ascii keys
+    p str
     str.map! do |item|
         if item>=65 && item<=90
             while key>(item-90).abs
                 key = key - (item-90).abs 
             end
+            item+key
 
 
 
         elsif item>=97 && item<=122
+            while key>(item-122).abs
+                key = key - (item-90).abs 
+            end
+            item+key
 
         else
             item
         end
 
     end
+    p str
 
 end
 
