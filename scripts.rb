51f6a22a625098a79be6790740d2dc46166f18aa
bepis <luqdek@gmail.com>
2021-01-11 19:51:01 +0000

    Added a while loop that ensures the encryption can wrap in the case of the key value being too hgih

--- scripts.rb
+++ scripts.rb
@@ -2,7 +2,12 @@ def caesar_cipher(str,key)
     str = str.bytes #convert the string into an array of the converted ascii keys
     str.map! do |item|
         if item>=65 && item<=90
-            
+            while key>(item-90).abs
+                key = key - (item-90).abs 
+            end
+
+
+
         elsif item>=97 && item<=122
 
         else
