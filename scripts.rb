3dbf67860cb4ff9693b40f76844f2dbaf70a2b51
bepis <luqdek@gmail.com>
2021-01-11 19:45:15 +0000

    Made the script leave the code if it is not a letter

--- scripts.rb
+++ scripts.rb
@@ -2,11 +2,13 @@ def caesar_cipher(str,key)
     str = str.bytes #convert the string into an array of the converted ascii keys
     str.map! do |item|
         if item>=65 && item<=90
-
+            
         elsif item>=97 && item<=122
-        
+
+        else
+            item
         end
-        
+
     end
 
 end
